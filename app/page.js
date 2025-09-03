35eea0203bcf8b543914c0d0abdebd3c7445d741
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-09-03 23:18:48 +0600

    feat: add Radix UI components and utility functions
    - Updated package.json to include new dependencies for Radix UI components and utility libraries.
    - Created components.json for component configuration.
    - Implemented Button, Input, ScrollArea, Select, Sheet, Tabs, Textarea, and Tooltip components using Radix UI primitives.
    - Added utility function `cn` for class name merging using clsx and tailwind-merge.

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h2 className="text-3xl font-bold text-amber-200">Sadekur Rahman</h2>
+      <h2 className="text-3xl font-bold text-red-900">Sadekur Rahman</h2>
+      <h2 className="text-3xl font-bold text-yellow-500">Sadekur Rahman</h2>
     </div>
   );
 }
