cde1356097a270c9e0f419ae4fe93cc98dd5c441
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-09-08 23:23:47 +0600

    tailwind config file variables add, postcss file add, font fammily add for all layout and config global css

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h2 className="text-3xl font-bold text-red-900">Sadekur Rahman</h2>
-      <h2 className="text-3xl font-bold text-yellow-500">Sadekur Rahman</h2>
-    </div>
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500 font-primary">
+        Sadekur Rahman - Font Test
+      </h1>
+    </main>
   );
 }
