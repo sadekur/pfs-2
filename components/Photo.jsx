282d1c5a29dddeb69e1bca4da4351bf447d2247e
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-02-28 14:17:49 +0600

    Implement: Assetes

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,10 +1,25 @@
 "use client";
+import { motion } from 'framer-motion'
+import Image from 'next/image';
 import React from 'react'
 
 const Photo = () => {
   return (
-    <div>
-      gbdg
+    <div className='w-full h-full relative rounded-full bg-gradient-to-tr from-accent to-primary overflow-hidden'>
+      <motion.div>
+        <div>
+          <Image
+            src="/sadekur-rahman.png"
+            priority
+            quality={100}
+            fill
+            alt="Sadekur Rahman"
+            // width={400}
+            // height={400}
+            className="w-[298px] h-[298px] object-cover rounded-full"
+          />
+        </div>
+      </motion.div>
     </div>
   )
 }
