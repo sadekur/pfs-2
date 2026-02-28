c629487787520bd250f9f3afb19d65584ec7be21
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-02-28 14:39:34 +0600

    Add: Profile photo

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -5,9 +5,9 @@ import React from 'react'
 
 const Photo = () => {
   return (
-    <div className='w-full h-full relative '>
-      <motion.div>
-        <div className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]'>
+    <div className='w-full h-full relative'>
+      <motion.div initial={{opacity: 0}} animate={{opacity: 1,  transition: {delay: 2, duration: 0.4, ease: 'easeIn'}}}>
+        <div className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten'>
           <Image
             src="/assets/photo.png"
             priority
