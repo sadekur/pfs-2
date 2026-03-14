fcc2800cdfa33a99c7c6a6d319077e913a21422e
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-03-14 22:27:41 +0600

    Update: Services section with responsive

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -47,13 +47,13 @@ const Services = () => {
               return (
                 <div key={index} className="flex-1 flex flex-col justify-center gap-6 group cursor-pointer ">
                   <div className="w-full flex justify-between items-center">
-                    <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover">{service.num}</div>
-                    <Link href={service.href}>
-                      <BsArrowDownRight />
+                    <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
+                    <Link className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent hover:rotate-45 transition-all duration-500 flex justify-center items-center" href={service.href}>
+                      <BsArrowDownRight className="text-primary text-3xl" />
                     </Link>
                   </div>
-                  <h2 className="">{service.name}</h2>
-                  <p>{service.description}</p>
+                  <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.name}</h2>
+                  <p className="text-white/60">{service.description}</p>
                   <div className="border-b border-white/28 w-full"></div>
                 </div>
               );
