8bd8c8c352c676a9586ec53f243020766c6161fd
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-03-14 22:04:07 +0600

    Update: Services section with responsive

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -35,10 +35,11 @@ const Services = () => {
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+            className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
           >
             {services.map((service, index) => {
               return (
-                <div key={index}>
+                <div key={index} className="flex-1 flex flex-col justify-center gap-6 group cursor-pointer ">
                   <div>
                     <div>{service.num}</div>
                     <Link href={service.href}>
