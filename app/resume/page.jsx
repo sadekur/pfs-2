2dbb51f06234f1fa63a568e175d2fcc2422c3531
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-03-14 22:50:20 +0600

    Update: Info section with responsive

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,4 +1,59 @@
+import { title } from 'framer-motion/client';
 import React from 'react'
+
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+  FaReact,
+  FaNodeJs,
+  FaWordPress
+} from "react-icons/fa";
+
+import {
+SiTailwindcss,
+SiNextdotjs,
+SiFramer,
+} from "react-icons/si";
+
+const about = {
+  title: "About Me",
+  description: "I am a passionate web developer with expertise in creating modern, responsive websites and applications."
+},
+info = [
+  {
+    fieldName: "Name",
+    fieldValue: "Sadekur Rahman"
+  },
+  {
+    fieldName: "Phone",
+    fieldValue: "+880 1780664660"
+  },
+  {
+    fieldName: "Experience",
+    fieldValue: "4+ years"
+  },
+  {
+    fieldName: "Location",
+    fieldValue: "Dhaka, Bangladesh"
+  },
+  {
+    fieldName: "Nationality",
+    fieldValue: "Bangladeshi"
+  },
+  {
+    fieldName: "Email",
+    fieldValue: "BZM0G@example.com"
+  },
+  {
+    fieldName: "Freelance",
+    fieldValue: "Available for work"
+  },
+  {
+    fieldName: "Languages",
+    fieldValue: "Bengali, English"
+  },
+]
 
 const Resume = () => {
   return (
