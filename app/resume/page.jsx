02dc847175265b2c37daf877ac13c46f2acadc34
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-04-10 22:28:56 +0600

    Add: Resume section content

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -53,16 +53,26 @@ const about = {
 };
 
 const experience = {
-  icon: <FaWordPress />,
-  // icon: "assets/resume/badge.svg",
+  // icon: <FaWordPress />,
+  icon: "assets/resume/badge.svg",
   title: "My Experience",
   description:
     "I have worked on various projects, ranging from small business websites to large-scale web applications. My experience includes both frontend and backend development, allowing me to create seamless user experiences and robust server-side solutions.",
     items: [
       {
+        company: "IGL WEB SOLUTIONS LTD.",
+        position: "Jounior Web Developer",
+        duration: "June 2018 - Aug 2018",
+      },
+      {
         company: "Codexpert Inc.",
         position: "Testing Engineer",
         duration: "June 2020 - Dec 2020",
+      },
+      {
+        company: "Local Gov. Engineering Division (LGED)",
+        position: "Computer Executive",
+        duration: "Dec 2020 - Feb 2023",
       },
       {
         company: "Codexpert Inc.",
@@ -76,6 +86,43 @@ const experience = {
       },
     ],
 };
+
+const education = {
+  // icon: <FaWordPress />,
+  icon: "assets/resume/cap.svg",
+  title: "My Education",
+  description:
+    "I have a strong educational background in computer science, which has provided me with a solid foundation in programming, algorithms, and software development principles. I am committed to continuous learning and staying updated with the latest industry trends and technologies.",
+    items: [
+      {
+        institution: "Bangladesh University (BU)",
+        degree: "Bachelor of Science in Computer Science and Engineering",
+        passingYear: "2018",
+      },
+      {
+        institution: "Gobindaganj Govt. College",
+        degree: "Higher Secondary Certificate (HSC)",
+        passingYear: "2012",
+      },
+      {
+        institution: "Kocashahar High School",
+        degree: "Secondary School Certificate (SSC)",
+        passingYear: " 2010",
+      },
+    ],
+};
+
+const skills = {
+  // icon: <FaWordPress />,
+  icon: "assets/resume/skills.svg",
+  title: "My Skills",
+  description: "",
+  items: [
+    {
+      name: "HTML5",
+      icon: <FaHtml5 className="text-orange-500" />,
+    },
+  ]
 
 const Resume = () => {
   return <div>Resume</div>;
