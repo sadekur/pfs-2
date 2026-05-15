ce93649a8dc4fe7b2a68af833adfe39d17d70227
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-15 20:32:19 +0600

    Update: Refactor ScrollArea import and enhance experience section layout

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -25,7 +25,7 @@ import {
 
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from "@/components/ui/tooltip";
-import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
 
 const about = {
@@ -236,10 +236,10 @@ const Resume = () => {
               <p className="text-white/60 max-w-[600px] mx-auto xl:mx-0">{experience.description}</p>
             </div>
             <ScrollArea className="h-[400px]">
-              <ul>
+              <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
                 {experience.items.map((item, index) => {
                   return (
-                    <li key={index} className="text-white/60">
+                    <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center iteams-center lg:items-start gap-1">
                       <h4 className="text-xl font-bold">{item.position}</h4>
                       <p className="text-lg">{item.company}</p>
                       <p className="text-md">{item.duration}</p>
