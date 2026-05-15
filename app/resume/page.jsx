0e5b241616b2773f73501f5211bc1e52489930cf
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-15 21:47:11 +0600

    Add: education section

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -279,19 +279,19 @@ const Resume = () => {
           
           <TabsContent value="education" className="w-full">
             <div className="flex flex-col gap-[38px] text-center xl:text-left">
-              <h3 className="text-4xl font-bold">{experience.title}</h3>
-              <p className="text-white/60 max-w-[600px] mx-auto xl:mx-0">{experience.description}</p>
+              <h3 className="text-4xl font-bold">{education.title}</h3>
+              <p className="text-white/60 max-w-[600px] mx-auto xl:mx-0">{education.description}</p>
               <ScrollArea className="h-[400px]">
                 <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
-                  {experience.items.map((item, index) => {
+                  {education.items.map((item, index) => {
                     return (
                       <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
                         <span className="text-accent">{item.duration}</span>
-                        <h3 className="text-xl max-w-[268px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                        <h3 className="text-xl max-w-[268px] min-h-[60px] text-center lg:text-left">{item.degree}</h3>
                         <div className="flex items-center gap-3 ">
                           <span className="w-[6px] h-[6px] rounded-full bg-accent">
                           </span>
-                          <p className="text-white/68">{item.company}</p>
+                          <p className="text-white/68">{item.institution}</p>
                         </div>
                       </li>
                     )
