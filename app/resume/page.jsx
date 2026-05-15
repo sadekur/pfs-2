a9adc69f98e7eed15b3e45311c188cf2f84ba244
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-15 22:23:43 +0600

    Update: Skills icons

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -316,7 +316,7 @@ const Resume = () => {
           </TabsContent>
           
           <TabsContent value="skills" className="w-full h-full">
-            <div className="flex flex-col gap-[30px]">
+            <div className="flex text-center flex-col gap-[30px]">
               <div className="flex flex-col gap-[30px] text-center xl:text-left ">
                 <h3 className="text-4xl font-bold">{skills.title}</h3>
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0 ">{skills.description}</p>
@@ -327,7 +327,7 @@ const Resume = () => {
                     <li key={index}>
                       <TooltipProvider delayDuration={100}>
                         <Tooltip>
-                          <TooltipTrigger>
+                          <TooltipTrigger className="">
                             <div className="text-4xl group-hover:text-accent transection-all duration-300">{item.icon}</div>
                           </TooltipTrigger>
                           <TooltipContent>
