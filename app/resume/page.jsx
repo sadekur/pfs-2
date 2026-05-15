f7ab34415f6fce0416c51cfa803c7183d344c787
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-15 21:45:08 +0600

    Add: education section

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -278,7 +278,27 @@ const Resume = () => {
           </TabsContent>
           
           <TabsContent value="education" className="w-full">
-            {/* Education content goes here */}da
+            <div className="flex flex-col gap-[38px] text-center xl:text-left">
+              <h3 className="text-4xl font-bold">{experience.title}</h3>
+              <p className="text-white/60 max-w-[600px] mx-auto xl:mx-0">{experience.description}</p>
+              <ScrollArea className="h-[400px]">
+                <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                  {experience.items.map((item, index) => {
+                    return (
+                      <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                        <span className="text-accent">{item.duration}</span>
+                        <h3 className="text-xl max-w-[268px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                        <div className="flex items-center gap-3 ">
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent">
+                          </span>
+                          <p className="text-white/68">{item.company}</p>
+                        </div>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </ScrollArea>
+            </div>
           </TabsContent>
           
           <TabsContent value="skills" className="w-full">
