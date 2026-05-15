6297aafb54539b43fe3c18d189c353b6d8967d63
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-15 22:01:05 +0600

    Improve: enhance skills section UI with structured layout and dynamic content

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -317,8 +317,8 @@ const Resume = () => {
                 <h3 className="text-4xl font-bold">{skills.title}</h3>
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0 ">{skills.description}</p>
               </div>
-              <ul>
-                {skills.items.map((item, index) => {
+              <ul className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px]">
+                {skills.skillList.map((item, index) => {
                   return (
                     <li key={index}>{item.name}</li>
                   );
