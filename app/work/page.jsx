61d2537d5ef9f2bc9dc054396a2c3f7f5dc88e39
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-12 20:37:53 +0600

    Add project data structure to Work component

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -8,6 +8,18 @@ import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from '@compone
 import Link from 'next/link';
 import Image from 'next/image';
 
+const projects = [
+  {
+    num: '01',
+    category: 'Frontend',
+    title: 'Project One',
+    description: 'Description of Project One',
+    stack: [{name: "Html 5"}, {name: "Css 3"}, {name: "JavaScript"}],
+    image: 'assets/work/thumb1.png',
+    github: 'https://github.com',
+    live: 'https://example.com',
+  },
+]
 const Work = () => {
   const [count, setCount] = useState(0);
 
