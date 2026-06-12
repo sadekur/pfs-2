e42cfb1fb9465c587c7822e07a0d7f5b201e1fa0
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-12 22:47:48 +0600

    Remove motion effects from Work component for simplified rendering

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -49,11 +49,6 @@ const Work = () => {
   };
 
   return (
-    <motion.div
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, esease: "easeIn" } }}
-    className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0"
-  >
     <div className="container mx-auto pt-[60px] xl:pt-[150px]">
       <div className="flex flex-col lg:flex-row xl:gap-[30px]">
         <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
@@ -124,7 +119,6 @@ const Work = () => {
         </div>
       </div>
     </div>
-    </motion.div>
   )
 }
 
