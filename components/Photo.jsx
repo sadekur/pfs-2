a6f824dfc15d3a92d3bf56396265ebd5473c9ac7
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-14 23:34:15 +0600

    Remove commented-out className for blur effect in Stairs component

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -14,7 +14,6 @@ const Photo = () => {
         }}
         className='relative flex justify-center items-center'
       >
-        {/* Circle SVG — renders first in flow */}
         <motion.svg
           className='w-[300px] h-[300px] xl:w-[500px] xl:h-[500px]'
           fill="transparent"
