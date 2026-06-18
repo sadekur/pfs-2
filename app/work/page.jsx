f86463902d93be27f4fe7f24151e32e62ff7302a
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-18 23:40:42 +0600

    Add WorkSliderButton component with animation effects

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -7,6 +7,7 @@ import {BsArrowRight, BsGithub} from 'react-icons/bs';
 import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from "@/components/ui/tooltip";
 import Link from 'next/link';
 import Image from 'next/image';
+import WorkSliderButton from '@/components/WorkSliderButton';
 
 const projects = [
   {
---
+++ components/WorkSliderButton.jsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { BsArrowRight } from "react-icons/bs";
+
+const WorkSliderButton = () => {
+    return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 2 }}
+            className="flex items-center gap-4"
+        >
+            <div className="w-[50px] h-[2px] bg-white"></div>
+            <BsArrowRight className="text-2xl" />
+        </motion.div>
+    );
+};
+
+export default WorkSliderButton;
