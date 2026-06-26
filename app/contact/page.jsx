86b1fb72c1c84d3dbb5afcdd25694f8b973576f7
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-26 21:31:54 +0600

    Refactor Contact component to include a contact form and display contact information

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
-import { Imput } from "@/components/ui/input";
+import { Imput, Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
