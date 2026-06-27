151da46af7aba0718e979a2fb2ceed343eba0b17
Sadekur <shadekur.rahman60@gmail.com>
2026-06-27 12:41:17 +0600

    Update both bg colors in contact/page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
     <div className="container mx-auto pt-[60px] xl:pt-[150px]">
       <div className="flex flex-col md:flex-row gap-[30px]">
         <div className="xl:h-[54%] order-2 xl:order-none">
-          <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+          <form className="flex flex-col gap-6 p-10 bg-[#1a1a2e] rounded-xl">
             <h3 className='text-4xl text-accent'>Lets Work Together</h3>
             <p className='text-white/60'>Feel free to reach out to me for any inquiries or collaborations. I'm always open to discussing new projects and opportunities.</p>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
@@ -68,7 +68,7 @@ const Contact = () => {
             {contactInfo.map((item, index) => {
               return (
                 <li key={index} className="flex items-center gap-6">
-                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#1a1a2e] text-accent rounded-md flex items-center justify-center">
                     <div className="text-[28px]">{item.icon}</div>
                   </div>
                   <div className="flex-1">
