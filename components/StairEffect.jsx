270138817846fc13af0d0160774b14b329579108
Sadekur <shadekur.rahman60@gmail.com>
2026-06-27 12:22:39 +0600

    Update: motion ui for StairEffect component

--- components/StairEffect.jsx
+++ components/StairEffect.jsx
@@ -1,28 +1,17 @@
 "use client"
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 import { usePathname } from 'next/navigation';
-import React from 'react'
 import Stairs from './ui/Stairs';
 
 const StairEffect = () => {
     const pathname = usePathname();
-  return (
-    <AnimatePresence mode="wait">
-        <div key={pathname}>
-            <div className='h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex'>
+    return (
+        <AnimatePresence mode="wait">
+            <div key={pathname} className='h-screen w-screen fixed top-0 left-0 pointer-events-none z-40 flex'>
                 <Stairs />
             </div>
-            <motion.div
-                initial={{ opacity: 1 }}
-                animate={{ 
-                    opacity: 0,
-                    transition:{ delay: 1, duration: 0.4, ease: "easeInOut" }
-                 }}
-                className="w-screen h-screen fixed top-0 left-0 right-0 bg-primary pointer-events-none"
-            />
-        </div>
-    </AnimatePresence>
-  )
+        </AnimatePresence>
+    )
 }
 
 export default StairEffect
