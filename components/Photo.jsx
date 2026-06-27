b663dfd8790f7bfb013d29786b82262e413720a1
Sadekur <shadekur.rahman60@gmail.com>
2026-06-27 12:40:49 +0600

    update Photo.jsx SVG ring stroke

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -25,7 +25,7 @@ const Photo = () => {
             cx="253"
             cy="253"
             r="250"
-            stroke="#00ff99"
+            stroke="#8b5cf6"
             strokeWidth="4"
             strokeLinecap="round"
             strokeLinejoin="round"
