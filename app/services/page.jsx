e3efe0fe9e6939ff5bbe3dae744bd0a4a047ac84
Sadekur <shadekur.rahman60@gmail.com>
2026-06-28 20:29:49 +0600

    fix: remove motion wrapper from Services component for improved performance

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -34,11 +34,7 @@ const Services = () => {
   return (
     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
       <div className="container mx-auto pt-[60px] xl:pt-[150px]">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
-            className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
-          >
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-[60px]">
             {services.map((service, index) => {
               return (
                 <div key={index} className="flex-1 flex flex-col justify-center gap-6 group cursor-pointer ">
