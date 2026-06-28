e99e4e5490b8858f684845a31fe312a203f03241
Sadekur <shadekur.rahman60@gmail.com>
2026-06-28 20:31:44 +0600

    remove the unused motion imports from work and contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,5 @@
 "use client"
 import React from 'react'
-
-import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { Imput, Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
